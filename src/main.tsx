78782e78babb5a20e48cfeca2ab2d193570887ba
Serhii Bova <serschgold@gmail.com>
2024-10-07 17:51:35 +0300

    hw02

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
 
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
         <App /> {" "}
