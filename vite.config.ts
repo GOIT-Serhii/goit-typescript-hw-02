8d397134c9787d5d93e085bb257f55f1a30a5736
Serhii Bova <serschgold@gmail.com>
2024-10-08 11:14:30 +0300

    hw02

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-typescript-hw-02/",
   plugins: [react()],
   build: {
     sourcemap: true,
